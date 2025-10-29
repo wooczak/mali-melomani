05b615a8a02111b62679fbf27890a34f80f6db40
rafalvu <rafal.walczewski00@gmail.com>
2025-10-29 19:24:13 +0100

    sorting out branches, making the ewbsite more responsive and disabling the game on mobile devices

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        instrukcja: './instrukcja-gry.html',
+        ciekawostki: './ciekawostki.html',
+      },
+    },
+  },
 })
